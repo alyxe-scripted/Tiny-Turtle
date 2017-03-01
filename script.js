650cc18071492c5833ffdf1214faf8aff0954d2e
Luis Angel Dilone <ldilone@aclanyc.com>
2017-03-01 19:45:35 +0000

    Make Tiny Turtle Build House

---
+++ script.js
@@ -0,0 +1,67 @@
+    TinyTurtle.apply(window); 
+    forward(30);
+    
+    left(90);
+    forward(10);
+    right(150);
+    forward(50);
+    
+    right(60);
+    forward(50);
+    right(150);
+    forward(10);
+    left(90);
+    
+    forward(30);
+    right(90);
+    forward(66);
+    right(90);
+    forward(30);
+    
+    right(90);
+    forward(33);
+    left(90);
+    stamp();
+    right(90);
+    forward(33);
+    
+    right(90);
+    forward(30);
+    left(90);
+    forward(10);
+    right(90);
+    forward(5);
+    
+    left(90);
+    forward(10);
+    right(90);
+    forward(5);
+    left(90);
+    forward(10);
+    right(90);
+    forward(5);
+    
+    right(90);
+    forward(126);
+    right(90);
+    forward(5);
+    right(90);
+    forward(10);
+    left(90);
+    forward(5);
+    right(90);
+    forward(10);
+    left(90);
+    forward(5);
+    right(90);
+    
+    forward(30);
+    left(90);
+    forward(15);
+    right(90);
+    forward(23);
+    right(90);
+    forward(15);
+    
+    
+    
